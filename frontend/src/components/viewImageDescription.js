40c1d824a39a882660f6f25989811668dccb60a3
tanzila01 <tanzilahameed0@gmail.com>
2021-11-11 10:57:45 +0500

    image view description updated

--- frontend/src/components/viewImageDescription.js
+++ frontend/src/components/viewImageDescription.js
@@ -6,6 +6,7 @@ import "./viewImageDescription.css"
 import ImageZoom from './imageZoom';
 
 function ViewImageDescription({product}) {
+    //just pushed
 
     const[image, setImage] = useState("")
     const[products,setProducts] = useState({})
